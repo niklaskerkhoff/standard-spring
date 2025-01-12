53b15bbc6c1365835340b185ab894a5875a0e7c2
Niklas Kerkhoff <niklas.kerkhoff@icloud.com>
2025-01-12 19:28:39 +0100

    jitpack build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ publishing {
             from(components["java"])
             groupId = "com.github.niklaskerkhoff"
             artifactId = "standard-spring"
-            version = "1.0.0"
+            version = "0.0.1"
         }
     }
 }
